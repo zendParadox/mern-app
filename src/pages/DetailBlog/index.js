7b33a21961ae29526f71374e6fc3e00aa1ccab9a
zendParadox <raflirama7446@gmail.com>
2024-07-27 14:49:37 +0700

    Updated: DetailBlog component to include image, title, author, and blog content

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { LoginBg } from "../../assets";
 
 const DetailBlog = () => {
   return (
     <div>
-      <p>Detail Blog</p>
+      <div className="container mx-auto ">
+        <div className="mb-10">
+          <img
+            src={LoginBg}
+            alt="thumb"
+            srcset=""
+            className="w-full h-80 object-cover"
+          />
+          <div className="my-4">
+            <h3 className="text-xl font-semibold">Title Blog</h3>
+            <p className="text-sm text-slate-500">Author - Date Post</p>
+          </div>
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatum ea illum debitis eos aliquid sed vero, cupiditate omnis
+              nostrum. Fuga illo animi nulla magni assumenda nostrum cum
+              voluptas unde, aspernatur quod provident explicabo alias
+              accusantium harum? Magnam asperiores eaque consequuntur? Enim
+              aperiam, expedita eum harum numquam possimus excepturi? Maiores,
+              quia.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
